980e2317781c7acb7ca26399cfb0e15297e9c32b
Ankur Biswas <ankur25590655@gmail.com>
2018-02-21 14:18:51 +0530

    Add files via upload

---
+++ Swapping.py
@@ -0,0 +1,10 @@
+#swapping of two variables
+
+a = input("What is the value of A\n")
+b=  input("what is the value of B\n")
+c = a
+a = b
+b = c
+print("After swapping the values (:")
+print(a)
+print(b)
